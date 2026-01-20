aba07d12fe1b051948d843a0b0dd146386b5728a
mallakm <mallakm@educ.college-montreal.qc.ca>
2026-01-20 12:08:41 -0500

    27 nov

--- Exercice2.py
+++ Exercice2.py
@@ -14,8 +14,8 @@ rndm_profession = str
 
 
 #                                                   #    strg, dex, con, intel, wis, cha
-Carpenter = np.array([11, 11, 0, 0, 0, 0])          #      11,    11,   0,     0,   0,   0
-Stonemason = np.array([11, 11, 0, 0, 0, 0])         #      11,    11,   0,     0,   0,   0
+Carpenter = np.array([10, 11, 0, 0, 0, 0])          #      10,    11,   0,     0,   0,   0
+Stonemason = np.array([11, 10, 0, 0, 0, 0])         #      11,    10,   0,     0,   0,   0
 Armorer = np.array([14, 12, 0, 0, 0, 0])            #      14,    12,   0,     0,   0,   0
 Blacksmith = np.array([14, 11, 0, 0, 0, 0])         #      14,    11,   0,     0,   0,   0
 Farmer = np.array([0, 0, 11, 0, 0, 0])              #      11,     0,  11,     0,   0,   0
@@ -24,20 +24,49 @@ Miller = np.array([12, 0, 10, 0, 0, 0])             #      12,     0,  10,     0
 Butcher = np.array([14, 0, 0, 0, 0, 0])             #      14,     0,   0,     0,   0,   0
 Baker = np.array([0, 0, 0, 0, 10, 0])               #       0,     0,   0,     0,  10,   0
 Cook = np.array([0, 14, 14, 0, 10, 0])              #       0,    14,  14,     0,  10,   0
-Beerbrewer = np.array([0, 0, 0, 0, 0, 0])           #       0,     0,   0,     0,   0,   0
+Beerbrewer = np.array([1, 0, 0, 0, 0, 0])           #       1,     0,   0,     0,   0,   0
 Innkeeper = np.array([0, 0, 0, 0, 0, 10])           #       0,     0,   0,     0,   0,  10
 Apothecary = np.array([0, 0, 0, 12, 13, 0])         #       0,     0,   0,    12,  13,   0
 Barber_surgeon = np.array([0, 0, 0, 14, 14, 0])     #       0,     0,   0,    14,  14,   0
-Shoemaker = np.array([0, 14, 0, 0, 0, 0])           #       0,    14,   0,     0,   0,   0
-Tailor = np.array([0, 14, 0, 0, 0, 0])              #       0,    14,   0,     0,   0,   0
+Shoemaker = np.array([0, 14, 0, 0, 1, 0])           #       0,    14,   0,     0,   1,   0
+Tailor = np.array([0, 14, 0, 1, 0, 0])              #       0,    14,   0,     1,   0,   0
 Architect = np.array([0, 0, 0, 14, 0, 0])           #       0,     0,   0,    14,   0,   0
 Clerk = np.array([0, 0, 0, 11, 0, 0])               #       0,     0,   0,    11,   0,   0
 Merchant = np.array([0, 0, 0, 12, 0, 14])           #       0,     0,   0,    12,   0,  14
 Bailiff = np.array([14, 0, 0, 0, 12, 0])            #      14,     0,   0,     0,  12,   0
 Alchemist = np.array([0, 0, 0, 14, 16, 0])          #       0,     0,   0,    14,  16,   0
 Astronomer = np.array([0, 0, 0, 16, 14, 0])         #       0,     0,   0,    16,  14,   0
-Candlemaker = np.array([0, 0, 0, 0, 0, 0])          #       0,     0,   0,     0,   0,   0
+Candlemaker = np.array([0, 0, 0, 0, 1, 0])          #       0,     0,   0,     0,   1,   0
 Scribe = np.array([0, 12, 0, 0, 13, 0])             #       0,    12,   0,     0,  13,   0
+
+prof_dict = {
+"Carpenter": [10, 11, 0, 0, 0, 0],
+"Stonemason": [11, 10, 0, 0, 0, 0],
+"Armorer": [14, 12, 0, 0, 0, 0],
+"Blacksmith": [14, 11, 0, 0, 0, 0],
+"Farmer": [11, 0, 11, 0, 0, 0],
+"Fisherman": [10, 0, 10, 0, 0, 0],
+"Miller": [12, 0, 10, 0, 0, 0],
+"Butcher": [14, 0, 0, 0, 0, 0],
+"Baker": [1, 0, 0, 0, 10, 0],
+"Cook": [0, 14, 14, 0, 10, 0],
+"Beerbrewer": [0, 0, 0, 0, 0, 0],
+"Innkeeper": [0, 0, 0, 0, 0, 10],
+"Apothecary": [0, 0, 0, 12, 13, 0],
+"Barber_surgeon": [0, 0, 14, 14, 0, 0],
+"Shoemaker": [0, 14, 0, 0, 1, 0],
+"Tailor": [0, 14, 0, 1, 0, 0],
+"Architect": [0, 0, 0, 14, 0, 0],
+"Clerk": [0, 0, 0, 11, 0, 0],
+"Merchant": [0, 0, 0, 12, 0, 14],
+"Bailiff": [14, 0, 0, 0, 12, 0],
+"Carpenter": [11, 11, 0, 0, 0, 0],
+"Alchemist": [0, 0, 0, 14, 16, 0],
+"Astronomer": [0, 0, 0, 16, 14, 0],
+"Candlemaker": [0, 0, 0, 0, 1, 0],
+"Scribe": [0, 12, 0, 0, 13, 0]
+}
+invert_prof_dict = {str(value): key for key, value in prof_dict.items()}
 
 opt_profession = [Carpenter, Stonemason, Armorer, Blacksmith, Farmer, Fisherman, Miller, Butcher, Baker, Cook, Beerbrewer, Innkeeper, Apothecary, Barber_surgeon, Shoemaker, Tailor, Architect, Clerk, Merchant, Bailiff, Alchemist, Astronomer, Candlemaker, Scribe]
 ctrl_opt_profession = []
@@ -50,8 +79,7 @@ def ctrl_rndm_profession():
         if (stats >= i).all():
             ctrl_opt_profession.append(i)
 
-
-
+    rndm_profession = random.choice(ctrl_opt_profession)
 
 
 def rndm_attribut():
@@ -75,7 +103,7 @@ def rndm_caracteristics():
     return rndm_name
 
 class NPC :
-    def __init__(self, strg, dex, con, intel, wis, cha, nom):
+    def __init__(self, strg, dex, con, intel, wis, cha, nom, profession):
         self.strg = strg
         self.dex = dex
         self.con = con
@@ -92,7 +120,7 @@ class NPC :
         #self.species = placeholder
         #self.ethnicity = placeholder
 
-        #self.profession = placeholder
+        self.profession = profession
 
 rndm_attribut()
 NPC.strg = valeur
@@ -114,4 +142,4 @@ print(NPC.strg, NPC.dex, NPC.con, NPC.intel, NPC.wis, NPC.cha, NPC.nom)
 stats = np.array([NPC.strg, NPC.dex, NPC.con, NPC.intel, NPC.wis, NPC.cha])
 
 ctrl_rndm_profession()
-print(ctrl_opt_profession)
+print(invert_prof_dict[str(rndm_profession)])
